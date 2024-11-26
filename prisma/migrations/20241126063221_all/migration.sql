118e155bb516a120d9bca2e6beb35f94dcb741e9
yogi nugroho <Ugroho@proton.me>
2024-11-26 13:32:52 +0700

    update migration

---
+++ prisma/migrations/20241126063221_all/migration.sql
@@ -0,0 +1,14 @@
+-- DropIndex
+DROP INDEX `Notification_id_key` ON `notification`;
+
+-- DropIndex
+DROP INDEX `OTP_id_key` ON `otp`;
+
+-- DropIndex
+DROP INDEX `Transaction_id_key` ON `transaction`;
+
+-- DropIndex
+DROP INDEX `TransactionInquiry_id_key` ON `transactioninquiry`;
+
+-- DropIndex
+DROP INDEX `User_id_key` ON `user`;
